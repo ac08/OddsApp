94aec33d3245bf6e361e0422c1ec778390bef2e9
andrewcircelli <andrewcircelli@gmail.com>
2020-09-29 16:06:31 -0400

    restructured World Series Odds ajax call and still not able to append info to page, dynamically

--- assets/logic.js
+++ assets/logic.js
@@ -4,65 +4,62 @@ $(document).ready(function() {
     
     // SportsData.io API - Endpoint URLs
     let bettingFuturesMarketURL = "https://api.sportsdata.io/v3/mlb/odds/json/BettingFuturesBySeason/2020POST?key=fae190a3b3c447529f443fead4937d4c"
+    // will need to dynamically generate this date
     let gameDate                = "2020-09-29";
     let gamesOddsbyDateURL      = "https://api.sportsdata.io/v3/mlb/odds/json/GameOddsByDate/" + gameDate + sportDataApiKey;
     let scheduleURL             = "https://api.sportsdata.io/v3/mlb/scores/json/Games/2020POST?key=fae190a3b3c447529f443fead4937d4c"
     let newsURL                 = "https://api.sportsdata.io/v3/mlb/scores/json/News?key=fae190a3b3c447529f443fead4937d4c"
     // SportsData.io API - Ajax Call - Populate World Series Odds
+    let worldSeriesOddsArr = [];
+    let ALWinnerArr = [];
+    let NLWinnerArr = [];
+
     $.ajax({
         "async": true,
         "crossDomain": true,
-        // "url": bettingFuturesMarketURL,
+        "url": bettingFuturesMarketURL,
         "method": "GET"
     }).done(function (response) {
-    // Declare Data Response 
-    let data = response[0];
-    console.log(data);
+        // Declare Data Response 
+        let data = response[0];
+        // Name of Betting Market Type and As Of Date - Can be placed on #worldSeriesWinner
+        let worldSeriesWinner     = data.BettingMarkets[11].BettingBetType;
+        let worldSeriesWinnerTime = data.BettingMarkets[11].Updated;
+        $('#worldSeriesWinner').text(worldSeriesWinner + " as of " + worldSeriesWinnerTime);
 
-    // Declaring variables outside of forEach, but not working inside or outside forEach loop
-    // Select list-group associated with #worldSeriesWinner
-    let listGroup = $("#worldSeriesWinnerListGroup");
-    // Create list-item for teamName
-    let listItem  = $("<li>");
-    // Create list-item-span for teamOdds
-    let listItemSpan = $("<span>");
+        // Configure Array for World Series Odds at DraftKings sportsbook (Id=7)
+        let draftKingsWSOddsArr  = [];
+        // Loop through Array for World Series Odds and push "line" to World Series Odds at DraftKings sportsbook Array
+        let tempArr = data.BettingMarkets[11].BettingOutcomes
+        tempArr.forEach(function(el) {
+            let sportsBook = el.SportsBook;
+            if (sportsBook.SportsbookID === 7) {
+                draftKingsWSOddsArr.push(el);
+            };
+        });
+        // Looop through Array for Worlds Series Odds at DraftKings sportsbook and push teamName and odds to functional array
+        draftKingsWSOddsArr.forEach(function(el) {
+            worldSeriesOddsArr.push({
+                teamName: el.Participant,
+                odds:     el.PayoutAmerican
+            });
+        });
+        // log to test results
+        console.log(worldSeriesOddsArr);
+    });
 
-    // Name of Betting Market Type and As Of Date - Can be placed on #worldSeriesWinner
-    let worldSeriesWinner = data.BettingMarkets[11].BettingBetType;
-    let worldSeriesWinnerTime = data.BettingMarkets[11].BettingBetType.Updated;
-    $('#worldSeriesWinner').text(worldSeriesWinner + " as of " + worldSeriesWinnerTime);
-    
-    // Configure Array for World Series Odds
-    let worldSeriesOddsArr = data.BettingMarkets[11].BettingOutcomes;
-    // Configure Array for World Series Odds at DraftKings sportsbook (Id=7)
-    let draftKingsWSOddsArr  = [];
-    // Loop through Array for World Series Odds and push "line" to World Series Odds at DraftKings sportsbook Array
+    // Dynamically generate World Series Odds Drop-Down Screen
     worldSeriesOddsArr.forEach(function(el) {
-        let sportsBook = el.SportsBook;
-        if (sportsBook.SportsbookID === 7) {
-            draftKingsWSOddsArr.push(el);
-        };
-    });
-    console.log(draftKingsWSOddsArr);
-    // Loop through Array for World Series Odds at DraftKings sportsbook and retrieve Team Name and AmPayout
-    draftKingsWSOddsArr.forEach(function (el) {
-        // Declare Variable for Team Name on each loop
-        let teamName  = el.Participant;
-        // Declare Variable for American Payout on each loop
-        let teamOdds  = el.PayoutAmerican;
-        // Set text of list-item to teamName
-        listItem.text(teamName); 
-        // Add classes to list-item
-        listItem.addClass("list-group-item d-flex justify-content-between align-items-center");
-        // Set text of list-item-span to teamOdds
-        listItemSpan.text(teamOdds); 
-        // Add classes to list-item-spand
-        listItemSpan.addClass("badge badge-dark");
-        // Append listItem to listGroup and listSpan to listItem
-        listGroup.append(listItem.append(listItemSpan));
+        let teamName  = el.teamName;
+        let odds      = el.odds;
+        let listGroup = $("#worldSeriesWinnerListGroup");
+        let listItem  = $("<li>");
+        listItem.text(teamName);
+        console.log(listItem);
+        listGroup.append(listItem);
     });
 
-    });
+
 
 // SportsData.io API - Ajax Call - Populate Pre-Game Odds
 // SportsData.io API - Ajax Call - Populate Live Odds
